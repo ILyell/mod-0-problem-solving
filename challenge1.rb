b5701eb9f6c2f39f2503036319f1c8f79c18e0d4
Ian Lyell <ian.lyell@yahoo.com>
2023-04-04 14:37:34 -0500

    Update Challenge 1

--- challenge1.rb
+++ challenge1.rb
@@ -1,11 +1,13 @@
 # 1. Return only strings with exactly 4 characters from an array of strings.
 # 2. An array of strings
-# 3. None
+# 3. Print every word? Return to an array?
 # 4. Create array["strings"], words.each do iteration, string.length == 4, return string.
 # 5. None yet
 # 6.
+# Setup initial array[]
 words = ["Data", "goal", "memory", "basket", "hope"]
 
+# Iterate over array for length of each word.
 words.each do |word|
     if word.length == 4; p word end
 end
---
+++ challenge2.rb
@@ -0,0 +1,2 @@
+# Print every word in lower case letters from and array of strings with a mix of upper/lower case.
+#
