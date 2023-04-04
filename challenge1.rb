68d422bf761ce5ba075a278d0523a51b429a855a
Ian Lyell <ian.lyell@yahoo.com>
2023-04-04 14:31:41 -0500

    Finish challenge 1

---
+++ challenge1.rb
@@ -0,0 +1,11 @@
+# 1. Return only strings with exactly 4 characters from an array of strings.
+# 2. An array of strings
+# 3. None
+# 4. Create array["strings"], words.each do iteration, string.length == 4, return string.
+# 5. None yet
+# 6.
+words = ["Data", "goal", "memory", "basket", "hope"]
+
+words.each do |word|
+    if word.length == 4; p word end
+end
