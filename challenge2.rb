33a2ffbec6e992a51e4f3b5f66855572a1780105
Ian Lyell <ian.lyell@yahoo.com>
2023-04-04 14:55:49 -0500

    Finish challenge 2

--- challenge2.rb
+++ challenge2.rb
@@ -1,2 +1,14 @@
-# Print every word in lower case letters from and array of strings with a mix of upper/lower case.
-#
+# 1. Print every word in lower case letters from and array of strings with a mix of upper/lower case.
+# 2. An array of strings
+# 3. None yet
+# 4. Create array[], array.each do |string|, p string.lowercase
+# 5. None yet
+# Code:
+
+# Create array of strings
+words = ["MiTCh", 'bEn', "TaBlE", "sINK"]
+
+# Iterate on array, make string lower case, print string
+words.each do |word|
+    p word.downcase
+end
